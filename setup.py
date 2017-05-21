81063802243aa42d2c8d4cb77e7bb88435511f69
Ritesh Kadmawala <ritesh@loanzen.in>
2017-05-21 10:08:30 +0530

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import sys
 from setuptools import setup
 
-version = '1.0.2'
+version = '1.0.3'
 
 if sys.argv[-1] == 'tag':
     os.system("git tag -a %s -m 'version %s'" % (version, version))
